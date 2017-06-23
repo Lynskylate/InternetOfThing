2e9462004f4237bf615973a7f887bd2c0ed841e3
lynskylate <lynskylate@gmail.com>
2017-06-23 19:41:03 +0800

    [features]完成了注册功能

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from .models import User
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, ValidationError
 from wtforms.validators import Required, Length, Email, EqualTo
 
 
@@ -10,13 +11,21 @@ class LoginForm(FlaskForm):
     submit = SubmitField("登录")
 
 
-class RegisterForm(FlaskForm):
+class RegisteForm(FlaskForm):
     email = StringField("邮箱", validators=[
                         Required(), Length(1, 64), Email()])
     username = StringField("用户名", validators=[
-        Required(), Length(1, 64)
+        Required(), Length(4, 64)
     ])
     password = PasswordField("密码", validators=[
                              Required(), EqualTo("password2", message="两次密码输入必须一致")])
     password2 = PasswordField("确认密码", validators=[Required()])
     submit = SubmitField("注册")
+
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError("已经存在的邮箱")
+
+    def validate_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError("已有的用户名")
--- app/views.py
+++ app/views.py
@@ -1,8 +1,8 @@
 from flask import render_template, flash, redirect, url_for
-from app import app
+from app import app, db
 from .models import User
-from .forms import LoginForm, RegisterForm
-from flask_login import login_required, login_user
+from .forms import LoginForm, RegisteForm
+from flask_login import login_required, login_user, current_user, logout_user
 
 
 @app.route("/")
@@ -12,23 +12,40 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        flash("你已经登录了")
+        return redirect(url_for("index"))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
         if user is not None and user.verify_password(form.password.data):
             login_user(user)
+            flash("登录成功")
             return redirect(url_for("index"))
-        flash("Invalid usrname or passwd")
+        flash("不正确的用户名或密码")
     return render_template("login.html", form=form)
 
 
 @app.route('/register', methods=['GET', 'POST'])
-def method_name():
-    form = RegisterForm()
+def register():
+    if current_user.is_authenticated:
+        flash("你已经登录了")
+        return redirect(url_for("index"))
+    form = RegisteForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data,
+                    username=form.username.data, password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash("注册成功")
+        login_user(user)
+        return redirect(url_for("index"))
     return render_template("register.html", form=form)
 
 
-@app.route("/secret")
+@app.route('/logout', methods=['GET', 'POST'])
 @login_required
-def secret():
-    return "Only authenticated users are allowed"
+def logout():
+    logout_user()
+    flash("注销成功")
+    return redirect(url_for("index"))
